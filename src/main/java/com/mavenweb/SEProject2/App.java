2aaf395ff28ee802c98a8cd83bb466a376dd2da5
SHARANYAREDDYPESARU <pesarusharanyareddy@gmail.com>
2024-11-16 14:46:43 +0530

    Update App.java

--- src/main/java/com/mavenweb/SEProject2/App.java
+++ src/main/java/com/mavenweb/SEProject2/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "P.Sharanya Reddy" );
+        System.out.println( "22BD1A663K" );
     }
 }
